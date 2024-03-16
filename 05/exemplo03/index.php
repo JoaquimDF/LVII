faf4f7266b2fc8decbe34aec387f80a39efda57b
JoaquimDF <162516949+JoaquimDF@users.noreply.github.com>
2024-03-15 21:50:43 -0300

    Update index.php

--- 05/exemplo03/index.php
+++ 05/exemplo03/index.php
@@ -1,16 +1,71 @@
-<?php
-	class Calculadora {
-		public function soma($x, $y) {
-            echo "A soma de $x + $y = " .($x + $y);
-        }
 
-        public function subtrai($x, $y) {
-            return $x - $y;
-        }
-	}
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Pessoas</title>
+</head>
+
+<body>
+	<h1>Cadastro de pessoas</h1>
+
+	<form method="post">
+		<div>
+
+			<label for="">CPF</label><br>
+
+			<input 
+				type="text" 
+				name="cpf" 
+				placeholder="Informe o número do CPF."
+				
+				>
+
+		</div>
+
+		<div>
+
+			<label for="">Nome</label><br>
+
+			<input type="text" name="nome" placeholder="Informe seu nome.">
+
+		</div>
+
+		<div>
+
+			<label for="">E-mail</label><br>
+
+			<input type="email" name="email" placeholder="Informe seu e-mail.">
+
+		</div>
+
+		<hr>
+
+		<button>Enviar</button>
+	</form>
+
+	<?php
+include 'pessoa.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$pessoa = new Pessoa();
 	
-	$calculadora = new Calculadora();
-	$resultado = $calculadora->subtrai(50,20);
-    print("Valor da operação é: " .$resultado);
+	$cpf = $_POST['cpf'];
+	$nome = $_POST['nome'];
+	$email = $_POST['email'];
 	
+	$pessoa->cpf = $cpf;
+	$pessoa->nome = $nome;
+	$pessoa->email = $email;
+
+	echo $pessoa->exibirDados();
+
+}
+
 ?>
+</body>
+
+</html>
