d21c4a971a2d7fd90874cd2b4df0c432c9d25d5b
JoaquimDF <162516949+JoaquimDF@users.noreply.github.com>
2024-10-23 17:19:54 -0300

    Add files via upload

--- 14/script/escolabd.sql
+++ 14/script/escolabd.sql
@@ -1,45 +1,31 @@
--- phpMyAdmin SQL Dump
--- version 5.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 28/09/2023 às 00:09
--- Versão do servidor: 10.4.11-MariaDB
--- Versão do PHP: 7.4.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Banco de dados: `escolabd`
 --
+DROP SCHEMA IF EXISTS `escolabd`;
 
+CREATE SCHEMA `escolabd`;
+
+USE `escolabd`;
 -- --------------------------------------------------------
 
 --
 -- Estrutura para tabela `perfis`
 --
 
-CREATE TABLE `perfis` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(50) NOT NULL
+CREATE TABLE `escolabd`.`perfis` (
+  `id` int NOT NULL PRIMARY KEY auto_increment,
+  `nome` varchar(50) NOT NULL 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Despejando dados para a tabela `perfis`
 --
 
-INSERT INTO `perfis` (`id`, `nome`) VALUES
+INSERT INTO `escolabd`.`perfis` (`id`, `nome`) VALUES
 (1, 'ADMINISTRADOR'),
-(2, 'GERENTE');
+(2, 'GERENTE'),
+(3, 'USUÁRIO COMUM');
 
 -- --------------------------------------------------------
 
@@ -47,57 +33,23 @@ INSERT INTO `perfis` (`id`, `nome`) VALUES
 -- Estrutura para tabela `usuarios`
 --
 
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
+CREATE TABLE `escolabd`.`usuarios` (
+  `id` int NOT NULL PRIMARY KEY auto_increment,
+  `email` varchar(255) NOT NULL ,
   `password` varchar(80) NOT NULL,
   `nome` varchar(100) NOT NULL,
-  `status` tinyint(1) NOT NULL
+  `status` tinyint NOT NULL,
+  `perfil_id` int NOT NULL,
+  FOREIGN KEY (perfil_id) REFERENCES perfis(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `email`, `password`, `nome`, `status`) VALUES
-(1, 'chico@email.com', '56daf002d328229cf4ef837e0b0ecbde', 'Chico', 1),
-(2, 'dora@email.com', '1f545a6d49bd6dc815ddd731d0c2a2ad', 'Dora', 1),
-(3, 'bob@email.com', '9f9d51bc70ef21ca5c14f307980a29d8', 'Bob', 1),
-(4, 'ademastor@email.com', 'c727b6e646efd75f99c2b75cb5b10570', 'Ademastor', 1);
+INSERT INTO `escolabd`.`usuarios` (`id`, `email`, `password`, `nome`, `status`, `perfil_id`) VALUES
+(1, 'chico@email.com', '56daf002d328229cf4ef837e0b0ecbde', 'Chico', 1, 1),
+(2, 'dora@email.com', '1f545a6d49bd6dc815ddd731d0c2a2ad', 'Dora', 1, 2),
+(3, 'bob@email.com', '9f9d51bc70ef21ca5c14f307980a29d8', 'Bob', 1, 2),
+(4, 'ademastor@email.com', 'c727b6e646efd75f99c2b75cb5b10570', 'Ademastor', 1, 2);
 
---
--- Índices de tabelas apagadas
---
-
---
--- Índices de tabela `perfis`
---
-ALTER TABLE `perfis`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices de tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de tabelas apagadas
---
-
---
--- AUTO_INCREMENT de tabela `perfis`
---
-ALTER TABLE `perfis`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT de tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
