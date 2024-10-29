b6e34b7cc8394367de2a2208bad2b09722d37c62
JoaquimDF <162516949+JoaquimDF@users.noreply.github.com>
2024-10-29 16:51:39 -0300

    Add files via upload

--- 16/index.php
+++ 16/index.php
@@ -1,20 +1,24 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 01 - MVC</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
+
 <body>
-    <h1>MVC - Cadastro de usuários e perfis</h1>
     <header>
+        <h1>MVC - Básico</h1>
+        <p>Exercício introdutório exemplificando o MVC nas tabelas usuários e perfil. </p>
+    </header>
     <nav>
-        <a href="index.php">Home</a>
+    <a href="index.php">Home</a>
         <a href="./view/usuarios/index.php">Usuários</a>
-        <a href="./view/perfis/index.php">Perfil</a>
+        <a href="#">Perfil</a>
+        
     </nav>
-</header>
 </body>
+
 </html>
-
-
